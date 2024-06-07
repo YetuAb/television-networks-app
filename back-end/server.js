ef077b0f584361bf439a2cbd6ebaf8f2811f8edf
Yetnayet Abebe <101176690+YetuAb@users.noreply.github.com>
2024-06-07 23:39:16 +0300

    Made CORS configuration to allow PUT and DELETE requests

--- back-end/server.js
+++ back-end/server.js
@@ -27,7 +27,7 @@ app.use(express.json());
 app.use(
   cors({
     origin: 'http://localhost:3000',
-    methods: ['GET', 'POST'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
   })
 );
